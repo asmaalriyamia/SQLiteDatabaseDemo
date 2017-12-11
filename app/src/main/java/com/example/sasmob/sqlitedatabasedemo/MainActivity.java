ca8503f80bed256df297fd839f795e2d83204bba
asmaalriyamia <33996600+asmaalriyamia@users.noreply.github.com>
2017-12-11 02:50:38 -0800

    MainActivity

--- app/src/main/java/com/example/sasmob/sqlitedatabasedemo/MainActivity.java
+++ app/src/main/java/com/example/sasmob/sqlitedatabasedemo/MainActivity.java
@@ -1,22 +1,35 @@
 package com.example.sasmob.sqlitedatabasedemo;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.BundleCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.TextView;
 
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+import com.example.sasmob.sqlitedatabasedemo.database_module.DatabaseHelperClass;
 import com.example.sasmob.sqlitedatabasedemo.database_module.Student;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 EditText name , department;
 String name1, department1;
+ListView listView;
+    Student student;
+    //ListView lst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,8 @@ String name1, department1;
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+       // lst = (ListView)findViewById(R.id.list_information);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -57,19 +72,44 @@ String name1, department1;
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressLint("WrongViewCast")
+
     public void save(View view) {
-     name = (EditText) findViewById(R.id.btn_save);
-     department = (EditText) findViewById(R.id.btn_display);
+     name = (EditText) findViewById(R.id.name);
+     department = (EditText) findViewById(R.id.department);
      name1= name.getText().toString();
      department1 = department.getText().toString();
-        Student student = new Student();
+       student = new Student();
         student.setName(name1);
         student.setDepartment(department1);
+        DatabaseHelperClass myhelper = new DatabaseHelperClass(this);
+        long l = myhelper.InsertStudentRecord(student);
+        if (l>0) {
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this,"Sorry Not inserted", Toast.LENGTH_SHORT).show();
+        }
 
 
     }
 
     public void display(View view) {
+        listView = (ListView) findViewById(R.id.list_information);
+        DatabaseHelperClass helper = new DatabaseHelperClass(this);
+        Cursor c=helper.getAllStudents();
+        String[] columnName = {"_id","name","department"};
+        int[] to= {R.id._id,R.id.nameStudent, R.id.departmentStudent};
+        SimpleCursorAdapter adapter= new SimpleCursorAdapter(this,R.layout.card_view,c,columnName,to);
+        listView.setAdapter(adapter);
+
+
+    }
+
+    public void delete(View view) {
+
+    }
+
+    public void update(View view) {
+        
     }
 }
